b338acc7a0f4d587fb790388980cdd2f6316cbc5
michaelcardoz27 <michael.cardoz27@gmail.com>
2025-12-11 10:10:18 +0400

    Update Silver_table_loads_transform.sql

--- scripts/silver/Silver_table_loads_transform.sql
+++ scripts/silver/Silver_table_loads_transform.sql
@@ -30,6 +30,30 @@ SELECT
 ROW_NUMBER () over (partition by cst_id order by cst_create_date desc) as flag_last
 FROM bronze.crm_cust_info
 ) t
-where flag_last = 1
-
-
+where flag_last = 1;
+--------------------------------------------------------------------------------------
+Insert into silver.crm_prod_info(
+	[prd_id],
+	[cat_id],
+	[prd_key],
+	[prd_nm],
+	[prd_cost],
+	[prd_line],
+	[prd_start_dt],
+	[prd_end_dt])
+select
+prd_id,
+Replace(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key,7,LEN(prd_key)) as prd_key,
+prd_nm,
+isnull(prd_cost,0) as prd_cost,
+CASE UPPER(TRIM(prd_line))
+	 WHEN 'M' then 'Mountain'
+	 WHEN 'R' then 'Road'
+	 WHEN 'S' then 'Other Sales'
+	 WHEN 'T' then 'Touring'
+	 Else 'n/a'
+END as prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over (partition by prd_key order by prd_start_dt)-1 as date) as prd_end_dt
+from bronze.crm_prod_info;
